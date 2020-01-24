25be47395b913c97eeed8e4f9d30dea7350d9c8c
Kevin Villalobos <kevinv15v@gmail.com>
2020-01-24 21:03:09 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,8 @@
-ary = [1,2,3,4,5]
-ary.each do |i|
-   puts i
+def square_array(array)
+  new_array = []
+  array.each{|a| new_array.push(a*a)}
+  return new_array
 end
+
+arr = [1, 2, 3, 4, 5]
+square_array(arr)
